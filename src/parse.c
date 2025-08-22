f9b84ba2fdac98b22c1b2770524821f1fc4385b1
Shayne Meyer <shayne.meyer@gmail.com>
2025-08-21 19:25:20 -0700

    updates to parse

--- src/parse.c
+++ src/parse.c
@@ -74,9 +74,9 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 	*headerOut = header;
 }
 
-int create_db_header(int fd, struct dbheader_t **headerOut) {
+int create_db_header(struct dbheader_t **headerOut) {
 	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-  if (header == -1) {
+  if (header == NULL) {
     printf("Malloc failed to create db header\n");
     return STATUS_ERROR;
   }
