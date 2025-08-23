1162fab70d0d6da70242b3e82ba01e1fbd77ed79
Shayne Meyer <shayne.meyer@gmail.com>
2025-08-23 07:53:31 -0700

    added add employee

--- src/parse.c
+++ src/parse.c
@@ -130,7 +130,12 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 	return 0;
 }
 
-int create_db_header(struct dbheader_t **headerOut) {
+int create_db_header(int fd, struct dbheader_t **headerOut) {
+	if (fd < 0) {
+		printf("Got a bad FD from the user\n");
+		return STATUS_ERROR;
+	}
+	
 	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
 
   if (header == (void*)-1) {
